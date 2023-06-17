1c8f5ddfd1db9ba6b1959e3590211b327d624c3d
M. Fiqri Ananda <80850827+MFiqri17@users.noreply.github.com>
2023-06-17 11:09:50 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+for (let n = 1; n <= 100; n++){
+    if (n % 5 == 0 && n % 3 == 0) {
+        console.log("Fizz Buzz")
+    } else if (n % 3 == 0) {
+        console.log("Fizz")
+    } else if (n % 5 == 0) {
+        console.log("Buzz")
+    } else {
+        console.log(n)
+    }
+}
